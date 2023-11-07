8ae71b83902ad7aa91933af8700498ced2d21b1b
Quiz5_Programacion_Java <diegoeslopez@gmail.com>
2023-11-07 15:32:50 -0500

    Hola

--- src/presentacion/IngresarPersona.java
+++ src/presentacion/IngresarPersona.java
@@ -13,25 +13,40 @@ public class IngresarPersona extends JFrame{
     private JTextField txtApellido;
     private JLabel lblApellido;
     private JButton btnGuardar;
+    private JLabel lblidioma;
+    private JComboBox cbmidioma;
+    private JCheckBox chbTerminos;
+    private JLabel lblGenero;
+    private JRadioButton rbtMasculino;
+    private JRadioButton rbtFemenino;
+    private JRadioButton rbtOtro;
+    private JRadioButton rbtPrefieroNoDecir;
+
     public IngresarPersona () {
         this.setTitle("Ventana Ingresar Persona");
         this.setContentPane(plnPirncipal);
         this.setVisible(true);
-        this.setSize(400,300);
+        this.setSize(800,500);
         btnGuardar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String codigo = txtCodigo.getText();
                 String nombre = txtNombre.getText();
                 String apellido = txtApellido.getText();
+                String idioma = cbmidioma.getSelectedItem().toString();
+                String aceptoTerminos = (chbTerminos.isSelected()) ? "Si" : "No";
+                String genero = (rbtMasculino.isSelected()) ? "Masculino" :
+                                (rbtFemenino.isSelected()) ? "Femenino" :
+                                        (rbtOtro.isSelected()) ? "Otro" :
+                                                (rbtPrefieroNoDecir.isSelected()) ? "PrefieroNoDecir": "";
 
-
-                String mensaje = "tus datos son: \n" +
-                                 " Codigo: " + codigo + "\n" +
+                String mensaje = "Tus Datos son: \n" +
+                                 "Codigo: " + codigo + "\n" +
                                  "Nombre: " + nombre + "\n" +
-                                 "Apelldio: " + apellido + "\n" ;
-
-                JOptionPane.showMessageDialog(btnGuardar, "Guardado con exito");
+                                 "Apelldio: " + apellido + "\n" +
+                                 "idioma: " + idioma + "\n" +
+                                 "AceptoTerminos" + aceptoTerminos;
+                JOptionPane.showMessageDialog(btnGuardar, mensaje);
             }
         });
     }
